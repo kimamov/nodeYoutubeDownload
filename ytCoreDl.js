e18ae9ce5bee12e9925caf177df49028a2838847
Nyannazz <kantemir.imam@gmail.com>
2019-05-02 20:20:21 +0200

    fixes

--- ytCoreDl.js
+++ ytCoreDl.js
@@ -5,6 +5,7 @@ const app=express()
 const port=5000;
 
 
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,60 +14,36 @@ app.use(function(req, res, next) {
 
 
 app.get('/dl',(req,res)=>{
-  getVideo(req.query.videolink,(id)=>{
-    res.send(`downloading ${id}`)
-  })
-  
-})
-
-app.get('/download',(req,res)=>{
-  //getVideo(req.query.videolink)
-  //res.stream(ytdl(req.query.videolink).pipe(fs.createWriteStream('video.flv')))
-  //res.download(`downloading ${req.query.videolink}`)
   let urlParts=req.query.videolink.split('/watch?v=')
   if(urlParts[1]===undefined){
     urlParts=req.query.videolink.split('outu.be/')
   }
   res.header('Content-Disposition', 'attachment; filename='+urlParts[1]+'.flv');
-  learnBuffer(req.query.videolink,(chunk)=>{
+  console.log(req.query.videolink)
+  let options=extractOptions(req.query.options)
+  console.log(options)
+  
+  getVideo(req.query.videolink,options,(chunk)=>{
     res.write(chunk)
   },()=>{
     res.end()
   })
   
 })
-app.get('/audio',(req,res)=>{
-  //getVideo(req.query.videolink)
-  //res.stream(ytdl(req.query.videolink).pipe(fs.createWriteStream('video.flv')))
-  //res.download(`downloading ${req.query.videolink}`)
-  let urlParts=req.query.videolink.split('/watch?v=')
-  if(urlParts[1]===undefined){
-    urlParts=req.query.videolink.split('outu.be/')
+
+extractOptions=(str)=>{
+  let output={}
+  if(str){
+    optionArr=str.split(',')
+    optionArr.forEach(option=>{
+    let entry=option.split(':')
+    output[entry[0]]=entry[1]
+  })
   }
-  res.header('Content-Disposition', 'attachment; filename='+urlParts[1]+'.mp3');
-  audioDownload(req.query.videolink,(chunk)=>{
-    res.write(chunk)
-  },()=>{
-    res.end()
-  })
-  
-})
-app.get('/audioonly',(req,res)=>{
-  //getVideo(req.query.videolink)
-  //res.stream(ytdl(req.query.videolink).pipe(fs.createWriteStream('video.flv')))
-  //res.download(`downloading ${req.query.videolink}`)
-  let urlParts=req.query.videolink.split('/watch?v=')
-  if(urlParts[1]===undefined){
-    urlParts=req.query.videolink.split('outu.be/')
-  }
-  res.header('Content-Disposition', 'attachment; filename='+urlParts[1]+'.mp3');
-  audioDownload(req.query.videolink,(chunk)=>{
-    res.write(chunk)
-  },()=>{
-    res.end()
-  })
-  
-})
+  return output
+}
+
+
 app.get('/info',(req,res)=>{
   //getVideo(req.query.videolink)
   //res.stream(ytdl(req.query.videolink).pipe(fs.createWriteStream('video.flv')))
@@ -79,30 +56,23 @@ app.get('/info',(req,res)=>{
     if(error){
       throw error
     }
-    //console.log(info)
-    res.send(JSON.stringify(info.formats))
+    console.log(info.thumbnail_url)
+    res.send(JSON.stringify(info))
   })
   
   
 })
  
-getVideo=(url,callback)=>{
-  const urlParts=url.split('/watch?v=')
-  console.log(urlParts[1])
-  ytdl(url)
-  .pipe(fs.createWriteStream(`${urlParts[1]}.flv`));
-  callback(urlParts[1])
-}
-learnBuffer=(url, callback, end)=>{
-  ytdl(url)
+getVideo=(url,options,callback, end)=>{
+  this.options=options? options: null
+  ytdl(url,this.options)
   .on('data',(chunk)=>{
     callback(chunk)
   }).on('end',()=>{
-    //console.log('finished')
     end()
   })
-  
 }
+
 audioDownload=(url, callback, end)=>{
   ytdl(url,{filter: "audioonly",quality: 'lowest'})
   .on('data',(chunk)=>{
