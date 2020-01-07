c4a92614c852a820289cb6bb159ac5cd121684b2
kantimam <kantemir.imam@gmail.com>
2020-01-07 18:38:01 +0100

    error handeling added to youtubesearch

--- search.js
+++ search.js
@@ -5,23 +5,23 @@ const router=express.Router();
 let filter;
 
 const searchVideo=(req, res)=>{
-    if(req.query.search){
-        ytsr.getFilters(req.query.search, function(err, filters) {
-            if(err) throw err;
+    if(req.query.query){
+        ytsr.getFilters(req.query.query, function(err, filters) {
+            if(err) return res.send("search failed", 500);
               filter = filters.get('Type').find(o => o.name === 'Video');
             ytsr.getFilters(filter.ref, function(err, filters) {
-              if(err) throw err;
+              if(err) return res.send("search failed", 500);
                 var options = {
-                    limit: 10,
+                    limit: req.query.amount || 10,
                     nextpageRef: filter.ref,
                 }
                 ytsr(null, options, function(err, searchResults) {
-                    if(err) throw err;
-                    return res.send(searchResults);
+                    if(err) return res.send("search failed", 500);
+                    return res.send("searchResults");
                 });
               });
           });
-    }else res.send("search string is required")
+    }else res.send("search string is required", 404)
     
 }
 
