24911783bd10681ff66ed9b7f1667547f7848da0
Nyannazz <kantemir.imam@gmail.com>
2020-07-06 16:10:08 +0200

    lovely youtube changed their layout again. added error messages for failed scraping until i fix it

--- searchSimple.js
+++ searchSimple.js
@@ -1,16 +1,16 @@
-const searchYt=require("./searchYt")
+const searchYt = require("./searchYt")
 
 
-const searchVideoSimple=(req, res)=>{
-  if(req.query.q){
-      searchYt(req.query.q, req.query.page).then(data=>{
-        return res.status(200).send(data);
-      }).catch(e=>{
-        console.log(e)
-        return res.status(500).send("search failed");
-      })
-  }else res.status(404).send("search string is required")
-  
+const searchVideoSimple = (req, res) => {
+  if (req.query.q) {
+    searchYt(req.query.q, req.query.page).then(data => {
+      return res.status(200).send(data);
+    }).catch(e => {
+      console.log(e)
+      return res.status(500).send(e);
+    })
+  } else res.status(404).send("search param q with your search terms is required")
+
 }
 
-module.exports=searchVideoSimple
+module.exports = searchVideoSimple
--- searchYt.js
+++ searchYt.js
@@ -4,50 +4,55 @@ const request = require("request-promise");
 
 function searchYt(query, page) {
     return new Promise((resolve, reject) => {
-        if (!query || typeof query !== "string") return reject(new Error("string query is required"))
+        if (!query || typeof query !== "string") return reject(new Error("search param q with your search terms is required"))
 
         request({
-                uri: `https://www.youtube.com/results?search_query=${query}${page? `&page=${page}`: ""}`,
-            })
+            uri: `https://www.youtube.com/results?search_query=${query}${page ? `&page=${page}` : ""}`,
+        })
             .then(data => {
                 const $ = cheerio.load(data);
 
                 const videos = $('div.yt-lockup.yt-lockup-tile.yt-lockup-video.vve-check.clearfix', data);
+                console.log(videos.length)
+
                 if (videos.length) {
+
                     const videoList = [];
                     for (let i = 0; i < videos.length; i++) {
                         const listItems = $("li", videos[i]);
-                        const thumbnail=$("img", videos[i]).attr("data-thumb");
-                        const linkTitle=$("h3>a", videos[i]).first();
-                        const listData=[]
+                        const thumbnail = $("img", videos[i]).attr("data-thumb");
+                        const linkTitle = $("h3>a", videos[i]).first();
+                        const listData = []
 
-                        listItems.each(function(i, item){
-                            listData[i]=$(item).text()
+                        listItems.each(function (i, item) {
+                            listData[i] = $(item).text()
                         })
 
-                        const title=linkTitle.text();
-                        const link=linkTitle.attr("href");
+                        const title = linkTitle.text();
+                        const link = linkTitle.attr("href");
 
                         // check if the output array alrady has a video with this link
-                        if(videoList.findIndex(video=>video.link===link)===-1){
+                        if (videoList.findIndex(video => video.link === link) === -1) {
                             videoList.push({
                                 uploaded: listData[0],
                                 views: listData[1],
-                                thumbnail: thumbnail? thumbnail : $("img", videos[i]).attr("src"),
+                                thumbnail: thumbnail ? thumbnail : $("img", videos[i]).attr("src"),
                                 duration: $("span", videos[i]).first().text().trim("\n"),
                                 title: title,
                                 link: link
                             })
                         }
-                        
+
                     }
                     const currentPage = page || 1;
                     resolve({
                         query: query,
                         page: currentPage,
-                        nextPage: `https://www.youtube.com/results?search_query=${query}&page=${Number(currentPage)+1}`,
+                        nextPage: `https://www.youtube.com/results?search_query=${query}&page=${Number(currentPage) + 1}`,
                         videos: videoList
                     });
+                } else {
+                    reject("The scraper could not find any videos. Most likely lovely Youtube changed their layout again... Feel free to contact me at kantemir.imam@gmail.com if you care")
                 }
 
 
